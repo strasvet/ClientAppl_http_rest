90ec7b671471ab1892d20ddb7dcbb3241e890ba2
strasvet <sosnovsky_d@outlook.com>
2018-01-23 23:16:07 +0200

    3.Ver 0.001,8b properties->adding link to server
    2.Ver 0.001,5b
    Move classes
    Edit view item get user
    
    1.Ver 0.001b

--- src/main/java/com/company/view/item/items/CreateItem.java
+++ src/main/java/com/company/view/item/items/CreateItem.java
@@ -42,7 +42,8 @@ public class CreateItem extends Item {
         ItemResponse itemResponse =(ItemResponse) PostMan2.getPostman(Settings.url.get("create"),HttpMethod.POST, entity, Select.item);
 
         if(itemResponse!=null) {
-            inputOutput.put(itemResponse);
+            //inputOutput.put(itemResponse);
+            inputOutput.put(String.format(" Id:%s\n Status:%s\n CreateDate:%s\n Description:%s\n Type:%s\n Title:%s\n Author%s\n e-mail:%s\n ",itemResponse.getId(),itemResponse.getItemStatus(),itemResponse.getCreatedDate(),itemResponse.getDescription(),itemResponse.getItemType(),itemResponse.getTitle(),itemResponse.getUser().getLastName(),itemResponse.getUser().getEmail()));
         }else{
             //inputOutput.put("Is item id: \""+id+"\" not found..");
         }
